8fe916302757c80bff5d413384004a79716d668f
darimah <rufaida.kariemah@ui.ac.id>
2026-04-24 11:22:19 +0700

    19_Refine Bounds #1 Model 2

--- optimization/get_parameter_bounds.m
+++ optimization/get_parameter_bounds.m
@@ -17,18 +17,40 @@ switch model_id
         bounds_x.Vblood = mk(550, 750);
         bounds_k.Klv = mk(1.0, 2.0); bounds_k.Krv = mk(1.5, 4.5);
     case 2
-        bounds_x.Ees_lv = mk(0.7, 2.2); bounds_x.Ees_rv = mk(0.8, 1.6);
-        bounds_x.V0_lv  = mk(10, 25);   bounds_x.V0_rv  = mk(15, 30);
-        bounds_x.Alv    = mk(0.8, 1.2); bounds_x.Arv    = mk(0.8, 1.2);
-        bounds_x.Blv    = mk(0.015, 0.025); bounds_x.Brv = mk(0.015, 0.025);
-        bounds_x.Rao    = mk(0.12, 0.17); bounds_x.Cao = mk(0.173, 0.273);
-        bounds_x.Ras    = mk(0.9, 1.4); bounds_x.Cas = mk(0.75, 1.5);
-        bounds_x.Rvs    = mk(0.038, 0.058); bounds_x.Cvs = mk(12, 20);
-        bounds_x.Rpo    = mk(0.007, 0.012); bounds_x.Cpo = mk(0.6, 1.6);
-        bounds_x.Rap    = mk(0.138, 0.158); bounds_x.Cap = mk(0.103, 0.153);
-        bounds_x.Rvp    = mk(0.038, 0.058); bounds_x.Cvp = mk(12, 20);
-        bounds_x.Vblood = mk(550, 700);
-        bounds_k.Klv = mk(1.0, 2.0); bounds_k.Krv = mk(1.0, 3.0);
+        bounds_x.Ees_lv = mk(0.5, 2.8); 
+        bounds_x.Ees_rv = mk(0.4, 1.8);
+    
+        bounds_x.V0_lv  = mk(5, 35);   
+        bounds_x.V0_rv  = mk(10, 40);
+    
+        bounds_x.Alv    = mk(0.6, 1.4); 
+        bounds_x.Arv    = mk(0.6, 1.3);
+    
+        bounds_x.Blv    = mk(0.010, 0.035); 
+        bounds_x.Brv    = mk(0.010, 0.030);
+    
+        bounds_x.Rao    = mk(0.05, 0.25); 
+        bounds_x.Cao    = mk(0.10, 0.35);
+    
+        bounds_x.Ras    = mk(0.6, 2.0); 
+        bounds_x.Cas    = mk(0.20, 1.80);
+    
+        bounds_x.Rvs    = mk(0.02, 0.08); 
+        bounds_x.Cvs    = mk(8, 24);
+    
+        bounds_x.Rpo    = mk(0.005, 0.05); 
+        bounds_x.Cpo    = mk(0.4, 2.5);
+    
+        bounds_x.Rap    = mk(0.08, 0.8); 
+        bounds_x.Cap    = mk(0.05, 0.6);
+    
+        bounds_x.Rvp    = mk(0.02, 0.10); 
+        bounds_x.Cvp    = mk(8, 24);
+    
+        bounds_x.Vblood = mk(450, 900);
+    
+        bounds_k.Klv = mk(0.8, 2.4); 
+        bounds_k.Krv = mk(0.8, 3.5);
     case 3
         bounds_x.Ees_lv = mk(1.25, 4.75); bounds_x.Ees_rv = mk(0.1, 0.8);
         bounds_x.V0_lv  = mk(0, 10);      bounds_x.V0_rv  = mk(10, 60);
